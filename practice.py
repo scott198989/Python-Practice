7aa5335350085cf859cf29d86d76ecd705413d5d
Scott Tuschl <scotttuschl@Scotts-iMac.local>
2023-09-21 13:34:42 -0500

    moving on to first app

--- practice.py
+++ practice.py
@@ -20,3 +20,17 @@ print(a)
 print(b)
 b = 17
 print(b)
+
+longer_phrase = "Hello, {}. Today is {}."
+
+formated = longer_phrase.format("Rolf", "Monday")
+
+print(formated)
+
+name = input("what is your name?")
+
+size_input = input("How big is your house(in square feet): ")
+square_feet = int(size_input)
+square_meters = square_feet / 10.8
+print(f"This many {square_feet} equals this many {square_meters:.2}")
+
