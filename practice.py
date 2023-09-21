7bfd11a1888b916733bcefd38a5c010bcc250f90
Scott Tuschl <scotttuschl@Scotts-iMac.local>
2023-09-21 17:21:54 -0500

    finished for today

--- practice.py
+++ practice.py
@@ -1,36 +1,53 @@
-x = 15
+# x = 15
 
-price = 9.99
+# price = 9.99
 
-discount = 0.2
+# discount = 0.2
 
-result = price * (1 - discount)
+# result = price * (1 - discount)
 
-print(result)
+# print(result)
 
-name = "Rolf"
+# name = "Rolf"
 
-print(name)
+# print(name)
 
-print(name * 2)
+# print(name * 2)
 
-a = 25
-b = a
-print(a)
-print(b)
-b = 17
-print(b)
+# a = 25
+# b = a
+# print(a)
+# print(b)
+# b = 17
+# print(b)
 
-longer_phrase = "Hello, {}. Today is {}."
+# longer_phrase = "Hello, {}. Today is {}."
 
-formated = longer_phrase.format("Rolf", "Monday")
+# formated = longer_phrase.format("Rolf", "Monday")
 
-print(formated)
+# print(formated)
 
-name = input("what is your name?")
+# name = input("what is your name?")
 
-size_input = input("How big is your house(in square feet): ")
-square_feet = int(size_input)
-square_meters = square_feet / 10.8
-print(f"This many {square_feet} equals this many {square_meters:.2}")
+# size_input = input("How big is your house(in square feet): ")
+# square_feet = int(size_input)
+# square_meters = square_feet / 10.8
+# print(f"This many {square_feet} equals this many {square_meters:.2}")
 
+# l = ["Bob", "Rolf", "Anne"]
+# t = ("Bob", "Rolf", "Anne")
+# s = {"Bob", "Rolf", "Anne"}
+
+# l[0] = "Smith"
+# l.append("Smith2")
+# # l.remove("Smith")
+# s.add("Smith")
+# print(s)
+
+# SETS
+
+friends = {"Bob", "Rolf", "Anne"}
+abroad = {"Bob", "Anne"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
