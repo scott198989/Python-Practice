9a9c8ca37809b4d60e3100e710dad74f6bcf9164
Scott Tuschl <scotttuschl@Scotts-iMac.local>
2023-09-22 19:13:16 -0500

    finished for today

--- practice.py
+++ practice.py
@@ -46,8 +46,112 @@
 
 # SETS
 
-friends = {"Bob", "Rolf", "Anne"}
-abroad = {"Bob", "Anne"}
+# friends = {"Bob", "Rolf", "Anne"}
+# abroad = {"Bob", "Anne"}
 
-local_friends = friends.difference(abroad)
-print(local_friends)
+# local_friends = abroad.difference(abroad)
+# print(local_friends)
+
+
+# local = {"Rolf"}
+# abroad = {"Bob", "Anne"}
+
+# friends = local.union(abroad)
+# print(friends)
+
+# art = {"Bob", "Jen", "Rolf", "Charlie"}
+# science = {"Bob", "Jen", "Adam", "Anne"}
+# both = art&science
+# print(both)
+
+
+# Booleans 
+
+# age = input("What is your age?")
+# age = int(age)
+
+# if age >= 21:
+#     print("You are old enough to vote, drink, and drive.  Just dont do all at the same time, ok?")
+# elif age >= 18:
+#     print("You can vote and you can drive. SWEET!")
+# elif age >= 16:
+#     print("Well, you can drive, so not bad. Gas prices are horrible though.")
+# else:
+#     print("Sorry little buddy, you cant vote, drink or drive. Go watch cartoons or something")
+
+
+# Conditional Statements (if, elif, else)
+
+# day_of_the_week = input("What day of the week is it?").lower()
+
+# if day_of_the_week == "friday": 
+#     print ("Its the weekend!")
+# else:
+#     print("Gotta do more Udemy classes!")
+
+
+# The "in" keyword
+
+# movies_watched = {"The Matrix", "Green Book", "Her"}
+# user_movie = input("Enter something you have watched recently.")
+
+# if user_movie in movies_watched:
+#     print(f"I've watched {user_movie} too!")
+# else:
+#     print("I havent seen that yet, NO SPOILERS!")
+
+# Magic Number Challenge / While loop
+
+# number = 7
+
+# while True:
+#     user_input = input("Enter 'Y/n' if you would like to play")
+#     if user_input == "n":
+#         break
+#     user_number = int(input("Guess our number: "))
+#     if user_number == number:
+#         print("You guessed correctly")
+#     elif abs(number - user_number) == 1:
+#         print("close, your off by one")
+#     else:
+#         print("Doh!")
+
+# For Loops
+
+# friends = ["Rolf", "Jen", "Bob", "Anne"]
+
+# for friend in friends:
+#     print(f"{friend} is my friend")
+
+# grades = [35, 67, 98, 100, 100]
+# total = 0
+# amount = len(grades)
+
+# for grade in grades:
+#     total += grade
+# print(total/amount)
+
+# Lists comprehension 
+
+# friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+# starts_s = [friend for friend in friends if friend.startswith("S")]
+
+# print(friends)
+# print(starts_s)
+# print(friends is starts_s)
+# print("friends: ", id(friends), "starts_s", id(starts_s))
+
+# Dictionaries
+
+# friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+# friend_ages["Bob"] = 20
+# print(friend_ages["Adam"])
+
+
+friends = [
+    {"name": "Rolf", "age": 24},
+    {"name": "Adam", "age": 30},
+    {"name": "Anne", "age": 27}
+]
+
+print(friends[1]["name"])
